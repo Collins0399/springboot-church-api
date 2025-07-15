07691abeb7e80f6bcd28ed73d4ff75cc25dc46ff
Collins0399 <kiprotichcollince34@gmail.com>
2025-07-15 16:03:00 +0300

    updated member service and controller

--- Church/src/main/java/com/techsavanna/Church/members/controllers/MemberController.java
+++ Church/src/main/java/com/techsavanna/Church/members/controllers/MemberController.java
@@ -7,7 +7,6 @@ import com.techsavanna.Church.members.services.MemberService;
 import com.techsavanna.Church.members.dtos.MemberResponseDto;
 import com.techsavanna.Church.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,59 +19,57 @@ public class MemberController {
     private MemberService memberService;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<MemberResponseDto>> createMember(@RequestBody MemberCreateDto dto) {
-        return ResponseEntity.ok(memberService.createMember(dto, null));
+    public ApiResponse<MemberResponseDto> createMember(@RequestBody MemberCreateDto dto) {
+        return memberService.createMember(dto, null);
     }
 
     @PutMapping("/{memberId}")
-    public ResponseEntity<ApiResponse<MemberResponseDto>> updateMember(
-            @PathVariable Long memberId,
-            @RequestBody MemberUpdateDto dto) {
-        return ResponseEntity.ok(memberService.updateMember(memberId, dto, null));
+    public ApiResponse<MemberResponseDto> updateMember(@PathVariable Long memberId, @RequestBody MemberUpdateDto dto) {
+        return memberService.updateMember(memberId, dto, null);
     }
 
     @DeleteMapping("/{memberId}")
-    public ResponseEntity<ApiResponse<Void>> deleteMember(@PathVariable Long memberId) {
-        return ResponseEntity.ok(memberService.deleteMember(memberId));
+    public ApiResponse<Void> deleteMember(@PathVariable Long memberId) {
+        return memberService.deleteMember(memberId);
     }
 
     @GetMapping("/{memberId}")
-    public ResponseEntity<ApiResponse<MemberResponseDto>> getByMemberId(@PathVariable Long memberId) {
-        return ResponseEntity.ok(memberService.getMemberById(memberId));
+    public ApiResponse<MemberResponseDto> getByMemberId(@PathVariable Long memberId) {
+        return memberService.getMemberById(memberId);
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse<List<MemberResponseDto>>> getAllMembers() {
-        return ResponseEntity.ok(memberService.getAllMembers());
+    public ApiResponse<List<MemberResponseDto>> getAllMembers() {
+        return memberService.getAllMembers();
     }
 
     @GetMapping("/by-email")
-    public ResponseEntity<ApiResponse<MemberResponseDto>> getMemberByEmail(@RequestParam String email) {
-        return ResponseEntity.ok(memberService.getMemberByEmail(email));
+    public ApiResponse<MemberResponseDto> getMemberByEmail(@RequestParam String email) {
+        return memberService.getMemberByEmail(email);
     }
 
     @GetMapping("/search-by-name")
-    public ResponseEntity<ApiResponse<List<MemberResponseDto>>> searchByFirstName(@RequestParam String namePart) {
-        return ResponseEntity.ok(memberService.searchByFirstName(namePart));
+    public ApiResponse<List<MemberResponseDto>> searchByFirstName(@RequestParam String namePart) {
+        return memberService.searchByFirstName(namePart);
     }
 
     @GetMapping("/by-marital-status")
-    public ResponseEntity<ApiResponse<List<MemberResponseDto>>> getByMaritalStatus(@RequestParam String maritalStatus) {
-        return ResponseEntity.ok(memberService.getMembersByMaritalStatus(maritalStatus));
+    public ApiResponse<List<MemberResponseDto>> getByMaritalStatus(@RequestParam String maritalStatus) {
+        return memberService.getMembersByMaritalStatus(maritalStatus);
     }
 
     @GetMapping("/by-department")
-    public ResponseEntity<ApiResponse<List<MemberResponseDto>>> getByDepartment(@RequestParam String departmentName) {
-        return ResponseEntity.ok(memberService.getMembersByDepartment(departmentName));
+    public ApiResponse<List<MemberResponseDto>> getByDepartment(@RequestParam String departmentName) {
+        return memberService.getMembersByDepartment(departmentName);
     }
 
     @GetMapping("/by-family-name")
-    public ResponseEntity<ApiResponse<List<MemberResponseDto>>> getByFamilyName(@RequestParam String familyName) {
-        return ResponseEntity.ok(memberService.getMembersByFamilyName(familyName));
+    public ApiResponse<List<MemberResponseDto>> getByFamilyName(@RequestParam String familyName) {
+        return memberService.getMembersByFamilyName(familyName);
     }
 
     @GetMapping("/by-baptism-status")
-    public ResponseEntity<ApiResponse<List<MemberResponseDto>>> getByBaptismStatus(@RequestParam BaptismStatus status) {
-        return ResponseEntity.ok(memberService.getMembersByBaptismStatus(status));
+    public ApiResponse<List<MemberResponseDto>> getByBaptismStatus(@RequestParam BaptismStatus status) {
+        return memberService.getMembersByBaptismStatus(status);
     }
 }
