260397793bf443f408abd03d19c7e005239fc13a
Collins0399 <kiprotichcollince34@gmail.com>
2025-08-01 14:03:24 +0300

    New

--- Church/src/main/java/com/techsavanna/Church/ChurchApplication.java
+++ Church/src/main/java/com/techsavanna/Church/ChurchApplication.java
@@ -2,6 +2,7 @@ package com.techsavanna.Church;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//testing pull/push
 
 @SpringBootApplication
 public class ChurchApplication {
